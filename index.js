37e51bc69320e2a3848355beae8cb4aa231be679
Carla Stickler <carlastickler@gmail.com>
2019-02-11 22:18:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 /* Enter the code to remove the main node element under this comment */
-
+const main = document.querySelector('main')
+main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1')
+newHeader.innerText = "Carla is a cool person"
+document.body.appendChild("h1")
+
+
+// for (let i = 0; i < 3; i++) {
+//   let victory = 
+//   document.createElement('victory')
+//   victory.innerHTML = (i + 1).toString()
+//   newHeader.appendChile(victory)
+// }
