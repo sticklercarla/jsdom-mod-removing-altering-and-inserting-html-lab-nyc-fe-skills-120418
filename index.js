d5cc69f166a7d61b45812cf9a0894dcae7d012dc
Carla Stickler <carlastickler@gmail.com>
2019-02-12 00:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
 const main = document.querySelector('main')
 main.remove()
+let yourName = "Carla"
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1')
 newHeader.setAttribute('id', 'victory')
-newHeader.innerText = "Carla "is the champion"
+newHeader.innerText = yourName + "is the champion"
 document.body.appendChild("h1")
 
 
