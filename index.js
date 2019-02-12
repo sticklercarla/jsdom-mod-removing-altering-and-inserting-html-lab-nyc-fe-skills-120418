8717d46082a0647f212628be544ea9c076d589aa
Carla Stickler <carlastickler@gmail.com>
2019-02-12 00:19:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const main = document.querySelector('main')
 main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1')
-newHeader.createAt
+newHeader.setAttribute('id', )
 newHeader.innerText = "Carla is a cool person"
 document.body.appendChild("h1")
 
