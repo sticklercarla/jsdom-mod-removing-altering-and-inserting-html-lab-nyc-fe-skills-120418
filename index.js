8ce630a3ad7f123ae2a7582762671bddbd47d006
Carla Stickler <carlastickler@gmail.com>
2019-02-12 00:27:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 /* Enter the code to remove the main node element under this comment */
-
+const main = document.querySelector('main')
+main.remove()
+let yourName = "Carla"
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerText = yourName + "is the champion"
+document.body.appendChild("h1")
+
+
+// for (let i = 0; i < 3; i++) {
+//   let victory = 
+//   document.createElement('victory')
+//   victory.innerHTML = (i + 1).toString()
+//   newHeader.appendChile(victory)
+// }
