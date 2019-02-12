2b5a3f782a0abafe9cd1e312ee406b84f8b5236e
Carla Stickler <carlastickler@gmail.com>
2019-02-12 00:17:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ main.remove()
 const newHeader = document.createElement('h1')
 newHeader.innerText = "Carla is a cool person"
 document.body.appendChild("h1")
-document.getElementByTagName.setAttribute('id', 'victory')
+document.getElementByTagName('h1').setAttribute('id', 'victory')
 
 // for (let i = 0; i < 3; i++) {
 //   let victory = 
